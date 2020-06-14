33350b199a619d44f4f8927d6502476fd395856c
phamngocthanhtrung <ngoc.thanh.trung.pham@iav.de>
2020-06-14 17:55:47 +0200

    initial commit

---
+++ src/main/java/de/trungpham/AppWithSpring/AppWithSpringApplication.java
@@ -0,0 +1,13 @@
+package de.trungpham.AppWithSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWithSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/trungpham/AppWithSpring/AppWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package de.trungpham.AppWithSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
